addff425c790da16f7a54da6167b14b879f44579
BearRyll <142635605+BearRyll@users.noreply.github.com>
2026-02-18 15:25:16 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
   trailingSlash: true,
-  reactStrictMode: true,
 };
 
 export default nextConfig;
